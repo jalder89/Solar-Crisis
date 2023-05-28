ce737c8e4351eafe385cf3c2341644416d9700bb
Jessica Alder <jessalder1989@gmail.com>
2023-05-28 14:36:18 -0700

    Player States & State Machine
    - Implemented basic player state machine class
    - Scaffolded Move and Idle States

---
+++ Assets/Solar Crisis/Scripts/Player/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerStateMachine stateMachine { get; private set; }
+
+    public PlayerIdleState idleState { get; private set; }
+    public PlayerMoveState moveState { get; private set; }
+
+    public Animator anim { get; private set; }
+
+    private void Awake()
+    {
+        stateMachine = new PlayerStateMachine();
+
+        idleState = new PlayerIdleState(this, stateMachine, "Idle");
+        moveState = new PlayerMoveState(this, stateMachine, "Move");
+    }
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+
+        stateMachine.Initialize(idleState);
+    }
+
+    private void Update()
+    {
+        stateMachine.currentState.Update();
+    }
+}
