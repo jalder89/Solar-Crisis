bcc78665897f5aec846ab799b3475bfda6173f12
Jessica Alder <jessalder1989@gmail.com>
2023-06-04 20:26:18 -0700

    UI & Mouse Interaction Update

---
+++ Assets/Solar Crisis/Scripts/UseIt.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class UseIt : MonoBehaviour
+{
+    public DialogueSystemTrigger dialogueTrigger;
+    public Collider2D triggerCollider;
+    public GameObject nameCanvas;
+    private Ray mouseRay;
+    
+
+    private void Update()
+    {
+
+        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        MouseOverCollider(mouseRay);
+    }
+
+    private void MouseOverCollider(Ray mouseRay)
+    {
+        if (triggerCollider.bounds.IntersectRay(mouseRay))
+        {
+            if (nameCanvas)
+            {
+                nameCanvas.SetActive(true);
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                dialogueTrigger.OnUse();
+            }
+        } else
+        {
+            if (nameCanvas)
+            {
+                nameCanvas.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            dialogueTrigger.OnUse();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.name == "Player")
+        {
+            nameCanvas.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            nameCanvas.SetActive(false);
+        }
+    }
+}
