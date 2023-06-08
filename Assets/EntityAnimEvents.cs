35ef2a64d66b537504b95329e98d4c94fae1b3d4
Jessica Alder <jessalder1989@gmail.com>
2023-06-07 22:30:07 -0700

    Brain room update

---
+++ Assets/EntityAnimEvents.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityAnimEvents : MonoBehaviour
+{
+    public void DisableEntity()
+    {
+        gameObject.SetActive(false);
+    }
+}
